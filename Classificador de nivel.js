9792b8078deae09c3bc7c9388d41a869b76ffa9e
Nicolas <nikaraujos3@gmail.com>
2024-01-15 14:06:42 -0300

    alteração no console.log

--- Classificador de nivel.js
+++ Classificador de nivel.js
@@ -3,22 +3,22 @@ let nivelDeexperiencia = 9000
 let XP = ""
 
 if(nivelDeexperiencia <= 1000){
-    console.log(XP="nivel Ferro")
+    XP="nivel Ferro"
 }else if(nivelDeexperiencia >= 1001 && nivelDeexperiencia<2001){
-    console.log(XP="nivel Bronze")
+    XP="nivel Bronze"
 }else if(nivelDeexperiencia >= 2001 && nivelDeexperiencia<5001){
-    console.log(XP="nivel Prata")
+    XP="nivel Prata"
 }else if(nivelDeexperiencia >= 6001 && nivelDeexperiencia<7001){
-    console.log(XP="nivel Ouro")
+    XP="nivel Ouro"
 }else if(nivelDeexperiencia >= 7001 && nivelDeexperiencia<8001){
-    console.log(XP="nivel Platina")
+    XP="nivel Platina"
 }else if(nivelDeexperiencia >= 8001 && nivelDeexperiencia<9001){
-    console.log(XP="nivel Ascendente")
+    XP="nivel Ascendente"
 }else if(nivelDeexperiencia >= 9001 && nivelDeexperiencia<10001){
-    console.log(XP="nivel Imortal")
+    XP="nivel Imortal"
 }else
 {
-    console.log(XP="nivel Radiante")
+    XP="nivel Radiante"
 }
 
 const notificação = "Conquista alcançada!"
