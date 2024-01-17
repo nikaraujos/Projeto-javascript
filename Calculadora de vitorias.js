0ec4ca293b1afe19be46f5d9e4cffaae48bffb13
Nicolas <nikaraujos3@gmail.com>
2024-01-17 02:14:48 -0300

    Calculadoa de vitórias

---
+++ Calculadora de vitorias.js
@@ -0,0 +1,35 @@
+let nivelDoheroi = ""
+let numeroDevitorias = 112
+let numeroDederrotas = 35
+let saldoDevitorias
+let resultado = contar(numeroDevitorias, numeroDederrotas)
+
+
+function contar(numeroDevitorias, numeroDederrotas){
+    return numeroDevitorias - numeroDederrotas
+}
+
+   if(resultado <= 10){
+        nivelDoheroi = "Ferro"
+    }
+    else if(resultado >= 10 && resultado <= 21){
+        nivelDoheroi = "Bronze"
+    }
+    else if(resultado >= 21 && resultado <= 51){
+        nivelDoheroi = "Prata"
+    }
+    else if(resultado >= 51 && resultado <= 81){
+        nivelDoheroi = "Ouro"
+    }
+    else if(resultado >= 81 && resultado <= 91){
+        nivelDoheroi = "Diamante"
+    }
+    else if(resultado >= 91 && resultado <= 101){
+        nivelDoheroi = "Lendario"
+    }
+    else{
+        nivelDoheroi = "Imortal"
+    }
+
+    console.log("O heroi tem saldo de " + resultado + " vitórias e está no nível de " + nivelDoheroi)
+
--- Classificador de nivel.js
+++ Classificador de nivel.js
@@ -3,22 +3,22 @@ let nivelDeexperiencia = 9000
 let XP = ""
 
 if(nivelDeexperiencia <= 1000){
-    XP="nivel Ferro"
-}else if(nivelDeexperiencia >= 1001 && nivelDeexperiencia<2001){
-    XP="nivel Bronze"
-}else if(nivelDeexperiencia >= 2001 && nivelDeexperiencia<5001){
-    XP="nivel Prata"
-}else if(nivelDeexperiencia >= 6001 && nivelDeexperiencia<7001){
-    XP="nivel Ouro"
-}else if(nivelDeexperiencia >= 7001 && nivelDeexperiencia<8001){
-    XP="nivel Platina"
-}else if(nivelDeexperiencia >= 8001 && nivelDeexperiencia<9001){
-    XP="nivel Ascendente"
-}else if(nivelDeexperiencia >= 9001 && nivelDeexperiencia<10001){
-    XP="nivel Imortal"
+        XP="nivel Ferro"
+    }else if(nivelDeexperiencia >= 1001 && nivelDeexperiencia<2001){
+        XP="nivel Bronze"
+    }else if(nivelDeexperiencia >= 2001 && nivelDeexperiencia<5001){
+        XP="nivel Prata"
+    }else if(nivelDeexperiencia >= 6001 && nivelDeexperiencia<7001){
+        XP="nivel Ouro"
+    }else if(nivelDeexperiencia >= 7001 && nivelDeexperiencia<8001){
+        XP="nivel Platina"
+    }else if(nivelDeexperiencia >= 8001 && nivelDeexperiencia<9001){
+        XP="nivel Ascendente"
+    }else if(nivelDeexperiencia >= 9001 && nivelDeexperiencia<10001){
+        XP="nivel Imortal"
 }else
 {
-    XP="nivel Radiante"
+        XP="nivel Radiante"
 }
 
 const notificação = "Conquista alcançada!"
